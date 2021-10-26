d59292bc4754b0d80935e4b75cf49e22314f5fcf
dnjoe96 <donjoedbest@gmail.com>
2021-10-26 09:55:18 -0700

    Add the commandpath function prototype in shell.h header file

--- shell.h
+++ shell.h
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /* string manipulator functions */
 int _strcmp(char *str_1, char *str_2);
@@ -17,5 +20,6 @@ char **_strtok(char *str, const char *delim);
 
 /* getpath */
 char **getpath(void);
+char *commandpath(char *command);
 
 #endif /* !SHELL_H */
