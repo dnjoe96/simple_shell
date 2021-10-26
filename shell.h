ffb1ccc447ec3bc5fde3016633f0d8e74c2c35ed
Washington Obungu <tobungu@gmail.com>
2021-10-26 02:23:23 -0700

    Created project header file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+/* string manipulator functions */
+int _strcmp(char *str_1, char *str_2);
+unsigned int _strlen(char *str);
+char *_strdup(char *str);
+
+/* string_tokenizing*/
+char **_strtok(char *str);
+
+#endif /* !SHELL_H */
