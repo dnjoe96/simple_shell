59ffef6ab44b06a6aac90c4c0d01a9e5ed0e445d
Washington Obungu <tobungu@gmail.com>
2021-10-28 04:14:46 -0700

    Added a function that executes commands

---
+++ execute_commands.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+ * fork_wait_exec - executes commands
+ * @args: command to be executed
+ *
+ * Return: 0
+ */
+int fork_wait_exec(char **argv)
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error: Failed to fork process!");
+		free(argv);
+		return (0);
+	}
+
+	if (child_pid == 0)
+	{
+		/*printf("%s\n", argv[0]);*/
+		if (execve(argv[0], argv, NULL) == -1)
+			perror("Error");
+		exit(0);
+		/*free(argv);*/
+	}
+	else
+		wait(&status);
+	free(argv);
+
+	return (0);
+}
