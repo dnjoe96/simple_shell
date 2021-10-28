8704d6295b6069ec506a73e6c302bdd21db38ce3
dnjoe96 <donjoedbest@gmail.com>
2021-10-28 07:42:31 +0100

    Added function prototype in shell.h

--- shell.h
+++ shell.h
@@ -10,6 +10,23 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
+/**
+ * struct command - A string - function pair
+ *
+ * @buf:  the command
+ * @print: Special function implemented to print data of a given
+ * specifier to stdout
+ *
+ * Description: A specifier - function pair
+ */
+typedef struct
+{
+	char *buf;
+	int (*execute)(char **args);
+} command;
+
+
 /* string manipulator functions */
 int _strcmp(char *str_1, char *str_2);
 unsigned int _strlen(char *str);
@@ -21,5 +38,10 @@ char **_strtok(char *str, const char *delim);
 /* getpath */
 char **getpath(void);
 char *commandpath(char *command);
+
+/* built-in functions */
+int _cd(char **args);
+int _exitprog(char **args);
+int _help(char **args);
 
 #endif /* !SHELL_H */
