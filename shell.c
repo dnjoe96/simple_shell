d94dc5d275de08a2cfa168a0b55b8cf19171751a
dnjoe96 <donjoedbest@gmail.com>
2021-11-01 09:27:30 +0100

    Make shell operate in Non-interactive mode

--- shell.c
+++ shell.c
@@ -13,83 +13,99 @@ void handle_sigkill(__attribute__((unused)) int sig)
  */
 int main(__attribute__((unused)) int argc, char **argv)
 {
-	int i;
-	char *commands, *check_path;
-	/*char **argv;*/
-	char **path;
-	size_t bytes_read = 1;
-	ssize_t chars_read;
-	char *exit_cond = "exit";
+        /*int i;*/
+        char *commands;
+        /*char *check_path;*/
+        char **path;
+        size_t bytes_read = 1;
+        ssize_t chars_read;
+        char *exit_cond = "exit";
+        /*
+        command command_struct[] = {
+                {"exit", _exitprog},
+                {"cd", _cd},
+                {"help", _help},
+                {"env", print_env},
+                {"others", fork_wait_exec},
+        };
+        */
+        signal(SIGINT, handle_sigkill);
 
-	command command_struct[] = {
-		{"exit", _exitprog},
-		{"cd", _cd},
-		{"help", _help},
-		{"env", print_env},
-		{"others", fork_wait_exec},
-	};
+        path = getpath();
 
-	signal(SIGINT, handle_sigkill);
+        commands = malloc(sizeof(char *));
+	
+	if (getline(&commands, &bytes_read, stdin) != 0)
+        {
+                commands[_strlen(commands) - 1] = '\0';
 
-	path = getpath();
-	while (1 && argc == 1)
-	{
-		write(STDOUT_FILENO, "$ ", 2);
+                argv = _strtok(commands, " \t");
 
-		commands = malloc(sizeof(char *));
-		chars_read = getline(&commands, &bytes_read, stdin);
-		if (chars_read == -1)
-		{
-			perror("Error: Could not read file_stream");
-		}
+                parse(argv, path);
 
-		if (commands[0] == '\n')
-			continue;
-		
-		commands[_strlen(commands) - 1] = '\0';
+                return (0);
+        }
 
-		argv = _strtok(commands, " \t");
-		/*free(commands);*/
+        while (1 && argc == 1)
+        {
+                write(STDOUT_FILENO, "$ ", 2);
 
-		if (strcmp(argv[0], exit_cond) == 0)
-			break;
-		/*free(commands);*/
+                commands = malloc(sizeof(char *));
+                chars_read = getline(&commands, &bytes_read, stdin);
+                if (chars_read == -1)
+                {
+                        perror("Error: Could not read file_stream");
+                }
 
-		check_path = commandpath(argv[0], path);
-		/*printf("check_path = %s\n", check_path);*/
-		
-		for (i = 0; i < 5; i++)
-		{
-			if (strcmp(argv[0], command_struct[i].buf) == 0)
-			{
-				command_struct[i].execute(argv);
-				break;
-			}
+                if (commands[0] == '\n')
+                        continue;
 
-			if (i == 4)
-			{
-				if (check_path != NULL)
-				{
-					argv[0] = check_path;
-					/*fork_wait_exec(argv);*/
-					command_struct[i].execute(argv);
-				}
-				else
-					perror("Error");
-			}
-		}
-	/*	
-		if (commands != NULL)
-			free(commands);
-		if (argv != NULL)
-			free(argv);
-	*/
-	}
+                commands[_strlen(commands) - 1] = '\0';
+
+                argv = _strtok(commands, " \t");
+                /*free(commands);*/
+
+                if (strcmp(argv[0], exit_cond) == 0)
+                        break;
+                /*free(commands);*/
+ 		
+		parse(argv, path);
+                /*
+                check_path = commandpath(argv[0], path);
+                
+                for (i = 0; i < 5; i++)
+                {
+                        if (strcmp(argv[0], command_struct[i].buf) == 0)
+                        {
+                                command_struct[i].execute(argv);
+                                break;
+                        }
+
+                        if (i == 4)
+                        {
+                                if (check_path != NULL)
+                                {
+                                        argv[0] = check_path;
+                                
+                                        command_struct[i].execute(argv);
+                                }
+                                else
+                                        perror("Error");
+                        }
+                }
+                */
+        /*      
+                if (commands != NULL)
+                        free(commands);
+                if (argv != NULL)
+                        free(argv);
+        */
+        }
 
 	if (commands != NULL)
-		free(commands);
-	if (argv != NULL)
-		free(argv);
+                free(commands);
+        if (argv != NULL)
+                free(argv);
 
-	return (0);
+        return (0);
 }
