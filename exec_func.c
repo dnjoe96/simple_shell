664eabefb39848dca9ccc86d91e939be9d1a811d
Washington Obungu <tobungu@gmail.com>
2021-11-03 05:12:43 -0700

    Added _execute.c statements onto exec_func.c

--- exec_func.c
+++ exec_func.c
@@ -7,10 +7,11 @@
  * @path: paths from env variables
  * Return: return 0
  */
-int exec_func(char **argv, char **path)
+int exec_func(char **argv, char **path, char *prog)
 {
 	int i;
 	char *check_path;
+	struct stat st;
 
 	command command_struct[] = {
 		{"exit", _exitprog},
@@ -22,6 +23,7 @@ int exec_func(char **argv, char **path)
 
 	check_path = commandpath(argv[0], path);
 
+
 	for (i = 0; i < 5; i++)
 	{
 		if (strcmp(argv[0], command_struct[i].buf) == 0)
@@ -32,13 +34,29 @@ int exec_func(char **argv, char **path)
 
 		if (i == 4)
 		{
+			if (argv[0][0] == '.' || argv[0][0] == '/')
+			{
+				if (stat(argv[0], &st) == 0)
+				{
+					command_struct[i].execute(argv);
+					break;
+				}
+				else
+					check_path = NULL;
+			}
+
 			if (check_path != NULL)
 			{
 				argv[0] = check_path;
 				command_struct[i].execute(argv);
 			}
 			else
-				perror("Error");
+			{
+				write(STDERR_FILENO, prog, _strlen(prog));
+				write(1, " : ", 4);
+				write(1, argv[0], _strlen(argv[0]));
+				write(1, " : not found\n", 14);
+			}
 		}
 	}
 
