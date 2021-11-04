6ccbfd0fae656e3650cd8dc6bc4db13aa54b8f35
Washington Obungu <tobungu@gmail.com>
2021-11-04 07:29:40 -0700

    Handled non interactive mode

--- string_tokenizing.c
+++ string_tokenizing.c
@@ -14,17 +14,24 @@ char **_strtok(char *str, const char *delim)
 	char **argv;
 	int i = 0, array_size = 1024;
 
+	/*printf("Before first strtok function\n");*/
 	tokens = strtok(str, delim);
+	/*printf("After first strtok function\n");*/
 	argv = malloc(sizeof(char *) * array_size);
 	if (argv == NULL)
 		return (NULL);
 
+	/*printf("Before strtok loop\n");*/
+	if (tokens == NULL)
+		return (NULL);
 	while (tokens != NULL)
 	{
 		argv[i] = tokens;
 		tokens = strtok(NULL, delim);
 		i++;
+		/*printf("Within strtok loop\n");*/
 	}
+	/*printf("After strtok loop\n");*/
 	argv[i] = NULL;
 
 	return (argv);
